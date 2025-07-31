2cb9fda36e69db9a4ea47d91bf85d1f8aba833a1
Andres Agudelo <andresagudelo1006@gmail.com>
2025-07-31 10:51:47 -0500

    Refactoriza el componente GoogleAnalytics para simplificar la configuración de gtag.js, eliminando propiedades innecesarias y mejorando la claridad del código. Se actualiza el comentario para reflejar el uso de Google tag.

--- src/app/components/GoogleAnalytics.tsx
+++ src/app/components/GoogleAnalytics.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 export default function GoogleAnalytics() {
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google tag (gtag.js) */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=AW-17421502407"
         strategy="afterInteractive"
@@ -15,36 +15,7 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'AW-17421502407', {
-            page_title: 'Psicóloga Daniela Orozco Miami',
-            page_location: 'https://www.daniela-orozco.com',
-            custom_map: {
-              dimension1: 'user_type',
-              dimension2: 'location'
-            }
-          });
-          
-          // Enhanced Ecommerce for Google Ads
-          gtag('config', 'AW-17421502407', {
-            allow_enhanced_conversions: true,
-            conversion_id: 'AW-17421502407',
-            conversion_label: 'CONVERSION_LABEL'
-          });
-        `}
-      </Script>
-
-      {/* Google Ads Conversion Tracking */}
-      <Script id="google-ads-conversion" strategy="afterInteractive">
-        {`
-          gtag('event', 'page_view', {
-            page_title: 'Psicóloga Daniela Orozco Miami',
-            page_location: 'https://www.daniela-orozco.com',
-            custom_parameters: {
-              user_type: 'hispanic_miami',
-              location: 'miami_florida',
-              language: 'spanish'
-            }
-          });
+          gtag('config', 'AW-17421502407');
         `}
       </Script>
     </>
